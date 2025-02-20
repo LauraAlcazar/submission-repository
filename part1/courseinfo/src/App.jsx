d096f7b89c61fca272bb6e2c328e42ad2b11277d
LauraAG <kirtas91@gmail.com>
2025-02-20 17:21:06 +0100

    exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,44 +3,29 @@ import Content from './Content';
 import Total from './Total';
 
 const App = () => {
-  const course = 'Half Stack application development'
-  /*const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }*/
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  /*const exercises = [
-    {exercisesNumber: part1.exercises, exerciseTitle: part1.name}, 
-    {exercisesNumber: part2.exercises, exerciseTitle: part2.name}, 
-    {exercisesNumber: part3.exercises, exerciseTitle: part3.name}
-  ]*/
 
   return (
     <div>
-      <Header course={course} />
-      <Content exercises={parts} />
-      <Total exercises={parts} />
+      <Header course={course.name} />
+      <Content exercises={course.parts} />
+      <Total exercises={course.parts} />
     </div>
   )
 }
